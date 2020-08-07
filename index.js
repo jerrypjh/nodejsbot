8b691b27cb2de067493f68c692200958d9d01525
sudal <thwndk4243@naver.com>
2020-08-08 00:04:58 +0900

    bot upload

--- index.js
+++ index.js
@@ -7,14 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) => {
-  if(message.content === '!좌표 test') {
-    message.reply('정상');
-  }
-});
-
-client.on('message', (message) => {
-  if(message.content === '!좌표 수달') {
-    message.reply('정상');
+  if(message.content === 'ping') {
+    message.reply('test');
   }
 });
 
